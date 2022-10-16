ea05f36b42d791c88efceff8a176ce5039cfa7b6
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-10-16 16:33:45 -0300

    finish

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,14 +1,19 @@
 import { Negociacao } from "./negociacao";
 
 export class Negociacoes {
+  // Array<Negociacao> ===  Negociacao[]
   private negociacoes: Array<Negociacao> = [];
 
   adiciona(negociacao: Negociacao) {
     this.negociacoes.push(negociacao);
   }
 
-  lista(): Array<Negociacao> {
-    // Pegar cada item dessa lista e colocar dentro desta nova lista para criar imutabilidade
-    return [...this.negociacoes];
+  // ReadonlyArray<Negociacao> === readonly Negociacao[]
+  lista(): ReadonlyArray<Negociacao> {
+    // Pegar cada item dessa lista e colocar dentro desta nova lista para criar imutabilidade - com Array<Negociacao>
+    // return [...this.negociacoes];
+    // OU
+    // utilizamos ReadonlyArray
+    return this.negociacoes;
   }
 }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,12 +1,17 @@
 export class Negociacoes {
     constructor() {
+        // Array<Negociacao> ===  Negociacao[]
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
+    // ReadonlyArray<Negociacao> === readonly Negociacao[]
     lista() {
-        // Pegar cada item dessa lista e colocar dentro desta nova lista para criar imutabilidade
-        return [...this.negociacoes];
+        // Pegar cada item dessa lista e colocar dentro desta nova lista para criar imutabilidade - com Array<Negociacao>
+        // return [...this.negociacoes];
+        // OU
+        // utilizamos ReadonlyArray
+        return this.negociacoes;
     }
 }
